9eac70f60694916766d6d67996b3b056572f6760
facuG03 <facundogonzales1550@gmail.com>
2022-10-08 11:08:54 -0300

    db change

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ const { Sequelize } = require('sequelize')
 const modelPokemon = require('./models/Pokemon')
 const modelType = require('./models/Type')
 const {
-  DB_USER, DB_PASSWORD, DB_HOST
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = require('./config')
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`, {
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`, {
   logging: false,
   native: false
 })
