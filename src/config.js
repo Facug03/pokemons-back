7ae4702bf02692a337e697e6dafc4d657afd23d9
Facug03 <107658697+Facug03@users.noreply.github.com>
2022-11-03 08:53:29 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,9 @@ const PORT = process.env.PORT || 3001
 const DB_USER = process.env.DB_USER || 'postgres'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
 const DB_HOST = process.env.DB_HOST || 'localhost'
-const DB_PORT = process.env.DB_PORT || '5432'
 const DB_NAME = process.env.DB_NAME || 'pokemon'
+const DB_PORT = process.env.DB_PORT || '5432'
+
 
 module.exports = {
   PORT,
