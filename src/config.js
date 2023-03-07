f0a399e32335c6febab6287cc4d3323a4c1a0453
Facundo Gonzalez <107658697+Facug03@users.noreply.github.com>
2023-03-07 10:29:07 -0300

    change port in dev mode

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3001
 const DB_USER = process.env.DB_USER || 'postgres'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
 const DB_HOST = process.env.DB_HOST || 'localhost'
